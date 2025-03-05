ff7d4c918d05e300de8961350380144b95dd2097
Ryan Angle <ryan071727@gmail.com>
2025-03-05 11:13:50 -0800

    p2-4

--- race.c
+++ race.c
@@ -2,9 +2,11 @@
 #include "stat.h"
 #include "user.h"
 #include "condvar.h"
+#include "fcntl.h"
 //We want Child 1 to execute first, then Child 2, and finally Parent. 
 int main() { 
-    struct condvar cv; 
+    struct condvar cv;
+    int fd = open("flag", O_RDWR | O_CREATE);
     init_lock(&cv.lk);
     int pid = fork(); //fork the first child 
     if(pid < 0) { 
@@ -12,16 +14,24 @@ int main() {
     } else if (pid == 0) { 
         sleep(5);
         printf(1, "Child 1 Executing\n");
-        lock(&cv.lk); 
-        cv_signal(&cv);  
-        unlock(&cv.lk); 
+        lock(&cv.lk);
+        write(fd, "done", 4);
+        cv_signal(&cv);
+        unlock(&cv.lk);
     } else { 
         pid = fork(); //fork the second child 
         if(pid < 0) { 
             printf(1, "Error forking second child.\n"); 
         } else if(pid == 0) { 
             lock(&cv.lk); 
-            cv_wait(&cv);  
+            struct stat stats; 
+            fstat(fd, &stats); 
+            printf(1, "file size = %d\n", stats.size); 
+            while(stats.size <= 0){ 
+                cv_wait(&cv); 
+                fstat(fd, &stats); 
+                printf(1, "file size = %d\n", stats.size); 
+            } 
             unlock(&cv.lk); 
             printf(1, "Child 2 Executing\n"); 
         } else { 
@@ -33,6 +43,8 @@ int main() {
         printf(1, "Parent Executing\n"); 
         printf(1, "Parent exiting.\n"); 
         } 
-    } 
+    }
+    close(fd); 
+    unlink("flag");
     exit(); 
 } 
